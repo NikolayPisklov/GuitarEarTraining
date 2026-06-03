47221f47523fd47cd8e7b817cf35c23227f929d5
NikolayPisklov <koluandino@gmail.com>
2026-06-03 18:24:52 +0300

    Initial commit

---
+++ backend/GuitarTrainer/Model/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuitarTrainer.Model
+{
+    public class DbContext : IdentityDbContext<AppUser>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Exercise> Exercises { get; set; }
+    }
+}
